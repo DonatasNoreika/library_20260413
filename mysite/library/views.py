81b3fc4c71d7975ee021aac04a5594d490c00028
DonatasNoreika <donoras@gmail.com>
2026-04-13 19:30:07 +0300

    Aprašytas ir registruotas Author modelis

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, template_name="index.html")
+    context = {
+        'zmones': ['Tomas', "Rokas", "Ainis", "Dar kažkas"],
+    }
+    return render(request, template_name="index.html", context=context)
 
