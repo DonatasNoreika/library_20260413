bdc89cd082833f528ea52a4a6663acc80bb76076
DonatasNoreika <donoras@gmail.com>
2026-04-13 19:59:45 +0300

    Aprašyti ir registruoti likę modeliai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
 
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'isbn']
+    inlines = [BookInstanceInLine]
+
 admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance)
